e1f9f2230f2c76941f759132affac98b8f431147
Rapli27158 <rafli2006ahmad@gmail.com>
2024-12-06 18:34:05 +0700

    Add files via upload

---
+++ formula.js
@@ -0,0 +1,32 @@
+function getInput() {
+    var male = document.getElementById("male").value;
+    var female = document.getElementById("female").value;
+  
+    if (!male || !female) {
+      return "kosong";
+    }
+  }
+  
+  function resInput() {
+    document.getElementById("male").value = "";
+    document.getElementById("female").value = "";
+  }
+  
+  function jodohin() {
+    getInput();
+    if (getInput() == "kosong") {
+      alert("Input tidak boleh kosong!");
+    } else {
+      var male = document.getElementById("male").value;
+      var female = document.getElementById("female").value;
+      var batas = 100;
+      var hasil = Math.floor(Math.random() * batas);
+      if (hasil === 0) {
+        document.getElementById("result").innerHTML = "Maaf kamu bukan jodoh dia";
+      } else {
+        document.getElementById("result").innerHTML =
+          male + " dan " + female + " hasilnya : " + hasil + " %";
+      }
+      resInput();
+    }
+  }
